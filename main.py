c34a2b077b9936c9bf2e208e903d2261ecf2ebfb
ArmandoPa12 <said201133@gmail.com>
2024-11-13 22:50:39 -0400

    predict version 2 added

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def predict():
     
     # Leer la imagen desde la solicitud y convertirla a un objeto BytesIO
     file = request.files['image']
-    img = Image.open(BytesIO(file.read()))
+    img = Image.open(BytesIO(file.read())).convert("RGB") 
     img = img.resize((224, 224))  # Ajustar el tamaño de la imagen a (224, 224)
     img_array = image.img_to_array(img) / 255.0  # Escalar los píxeles a [0, 1]
     img_array = np.expand_dims(img_array, axis=0)  # Añadir una dimensión para representar un solo lote
@@ -61,6 +61,42 @@ def predict():
         "confidence": confidence*100
     })
 
+
+@app.route("/predictv2", methods=["POST"])
+def predictv2():
+    # Verificar que se haya enviado un archivo
+    if 'image' not in request.files:
+        return jsonify({"error": "No image provided"}), 400
+    
+    # Leer la imagen desde la solicitud y convertirla a un objeto BytesIO
+    file = request.files['image']
+    img = Image.open(BytesIO(file.read())).convert("RGB") 
+    img = img.resize((224, 224))  # Ajustar el tamaño de la imagen a (224, 224)
+    img_array = image.img_to_array(img) / 255.0  # Escalar los píxeles a [0, 1]
+    img_array = np.expand_dims(img_array, axis=0)  # Añadir una dimensión para representar un solo lote
+
+    confidence_threshold = float(request.form.get('confidence', 85)) / 100
+
+    # Realizar la predicción
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions)
+    predicted_label = class_indices[predicted_class]
+    confidence = float(predictions[0][predicted_class])  # Mantener el nivel de confianza como un valor entre 0 y 1
+
+    # Verificar si el nivel de confianza es suficiente
+    if confidence < confidence_threshold:
+        return jsonify({
+            "predicted_label": "No se reconoce ningún billete",
+            "confidence": f"{confidence * 100:.2f}%",
+            "umbral":confidence_threshold
+        })
+    else:
+        # Devolver la predicción con el nivel de confianza en porcentaje
+        return jsonify({
+            "predicted_label": predicted_label,
+            "confidence": f"{confidence * 100:.2f}%",
+            "umbral":confidence_threshold
+        })
 
 @app.route("/inspect", methods=["POST"])
 def inspect():
